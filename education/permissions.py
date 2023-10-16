63df6486ca14e988270230cb55444b6cf9ee4ea5
IgorK78 <i.kolegov78@gmail.com>
2023-10-16 22:06:38 +0300

    edit from 25.1 (correction)

--- education/permissions.py
+++ education/permissions.py
@@ -3,18 +3,16 @@ from rest_framework.permissions import BasePermission
 
 class EducationItemAccess(BasePermission):
     def has_permission(self, request, view):
-        print(request.method)
         if request.method == 'GET':
             return request.user.is_authenticated
         elif request.method == 'POST':
             return request.user.is_authenticated and not self.user_is_moderator(request)
-        elif request.method in ['PUT', 'PATCH', 'DELETE']:
+        elif request.method in ['PUT', 'PATCH']:
             return True
         else:
             return False
 
     def has_object_permission(self, request, view, obj):
-        print(request.method)
         if not request.user.is_authenticated:
             return False
 
